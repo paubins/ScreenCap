9e38b3cc7f81c86703491b45437658777e166c6c
Patrick Aubin <paubin12@gmail.com>
2017-09-26 10:37:54 -0700

    Initial Commit

---
+++ ScreenCap/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScreenCap
+//
+//  Created by Patrick Aubin on 9/26/17.
+//  Copyright © 2017 Patrick Aubin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ScreenCap/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScreenCap
+//
+//  Created by Patrick Aubin on 9/26/17.
+//  Copyright © 2017 Patrick Aubin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
